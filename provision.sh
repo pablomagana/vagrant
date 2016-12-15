65251b7e8830badb7bb39f7038f0746038fdf9e7
pablomagub <pablomagub@gmail.com>
2016-12-15 20:49:01 +0100

    vagrantfile provision.sh ampliado ejercicio 2

--- provision.sh
+++ provision.sh
@@ -1,2 +1,7 @@
 apt-get -y update
 apt-get -y install apache2
+apt-get -y install git
+rm -r /var/www
+mkdir -p /vagrant/www/jquery
+ln -s /vagrant/www /var/www
+git clone https://github.com/pablomagana/jQueryActividades.git /var/www/jquery
