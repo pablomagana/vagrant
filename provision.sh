705414e40b048bfd765318b363329923911a59a1
Pablo Magaña <pablomagub@gmail.com>
2016-12-19 18:47:57 +0000

    Delete provision.sh

--- provision.sh
+++
@@ -1,7 +0,0 @@
-apt-get -y update
-apt-get -y install apache2
-apt-get -y install git
-rm -r /var/www
-mkdir -p /vagrant/www/jquery
-ln -s /vagrant/www /var/www
-git clone https://github.com/pablomagana/jQueryActividades.git /var/www/jquery
